0f73b3611be2e2c4a7e2248671d601808ce322f1
Vegard Rørvik Solum <vrs@4subsea.com>
2020-10-19 13:03:34 +0200

    added code to campaigns api

--- fourinsight/campaigns/campaign.py
+++ fourinsight/campaigns/campaign.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from fourinsight.api.globalsettings import environment
 from .api import CampaignsAPI
 
@@ -10,8 +12,8 @@ class BaseCampaign:
         self._campaigns_api = CampaignsAPI(auth_session)
         self._campaign = self._campaigns_api.get_campaign(campaign_id)
         self._sensors = self._campaigns_api.get_sensors(campaign_id)
-        self._events = self._campaigns_api.get_sensors(campaign_id)
-        self._geotrack = {}   # Not available with the rest api yet
+        self._events = self._campaigns_api.get_events(campaign_id)
+        self._geotrack = self._campaigns_api.get_geotrack(campaign_id)
 
     def general(self):
         """Get general campaign info."""
@@ -21,33 +23,65 @@ class BaseCampaign:
         """Get weather (geotrack) information for the campaign."""
         return self._geotrack
 
-    # def sensors(self, value=None, by="Position"):
-    #     """Get a sensor by its position or name.
+    def events(self, value=None, by="Event Type"):
+        """Get the events by its event type.
 
-    #     Parameters
-    #     ----------
-    #     value, str (optional)
-    #         The position (or name) to find. If None, return all sensors.
-    #     by, str
-    #         What to find. Allowed values are `Position` or `Name`.
+        Parameters
+        ----------
+        value, str (optional)
+            The event type to find. If None, return all events.
+        by, str
+            What to find. Allowed values are `Event Type`.
 
-    #     Returns
-    #     -------
-    #     list of dicts
-    #         A list containing sensor dicts.
+        Returns
+        -------
+        list of dicts
+            A list containing event dicts. Results are sorted on `Start`.
 
-    #     """
-    #     if value is None:
-    #         return self._sensors
+        """
+        if value is None:
+            return self._events
 
-    #     sensors = self._filter_dict_value_by(self._sensors, value, by.capitalize())
-    #     if len(sensors) == 0:
-    #         raise RuntimeError("No sensors matches the criteria.")
-    #     return sensors
+        events = self._filter_dict_value_by(self._events, value, by)
+        if len(events) == 0:
+            raise RuntimeError("No events matches the criteria.")
 
-    # @staticmethod
-    # def _filter_dict_value_by(dict_, value, by):
-    #     return list(filter(lambda x: x[by] == value, dict_))
+        events = sorted(
+            events,
+            key=lambda x: x["Start"]
+            if x["Start"] else pd.to_datetime("1970-01-01T00:00:00+0000")
+            )
+        return events
+
+    def sensors(self, value=None, by="Position"):
+        """Get a sensor by its position or name.
+
+        Parameters
+        ----------
+        value, str (optional)
+            The position (or name) to find. If None, return all sensors.
+        by, str
+            What to find. Allowed values are `Position` or `Name`.
+
+        Returns
+        -------
+        list of dicts
+            A list containing sensor dicts.
+
+        """
+        if value is None:
+            return self._sensors
+
+        sensors = self._filter_dict_value_by(self._sensors, value, by.capitalize())
+        if len(sensors) == 0:
+            raise RuntimeError("No sensors matches the criteria.")
+        return sensors
+
+    @staticmethod
+    def _filter_dict_value_by(dict_, value, by):
+        return list(filter(lambda x: x[by] == value, dict_))
+
+    # def _get_campaign(self, campaign_id)
 
 
 class GenericCampaign(BaseCampaign):
