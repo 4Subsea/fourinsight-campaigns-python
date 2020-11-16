627e58902adc318788ac10c23c4e7d8789c0b0e8
Ali Cetin <ali.cetin@4subsea.com>
2020-11-16 07:57:04 +0100

    Update setup.py
    Add fourinsight-api as dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="fourinsight_campaigns",
+    name="fourinsight-campaigns",
     version="0.0.1",
     description="4insight campaigns",
     author="4Subsea",
@@ -9,6 +9,7 @@ setup(
     url="https://4insight.io/",
     packages=["fourinsight.campaigns"],
     install_requires=[
+        "fourinsight-api"
         "pandas",
     ],
     zip_safe=False,
