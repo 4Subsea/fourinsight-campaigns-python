560d7507380e321091b054c11ff8da7eb03acff1
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2021-08-13 08:39:13 +0200

    fix: data map

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,6 +29,23 @@ from .testdata.get_data_v11 import (
 )
 
 
+DATA_MAP = {
+    "/v1.1/campaigns": CAMPAIGNS_DATA,
+    "campaigns next link": CAMPAIGNS_NEXT_DATA,
+    "/v1.1/campaigns/1234/events": EVENTS_DATA,
+    "/v1.1/campaigns/1234/sensors": SENSORS_DATA,
+    "/v1.0/campaigns/1234/lowerstack": LOWERSTACK_DATA,
+    "/v1.1/campaigns/swimops": SWIMOPS_DATA,
+    "/v1.1/campaigns/1234/swimops": SWIMOPS_CAMPAIGN_DATA,
+    "/v1.1/campaigns/1234/logs": LOGS_DATA,
+    # "/v1.1/campaigns/1234/sensors/5678/channels": CHANNELS_DATA,
+    "/v1.1/campaigns/1234/sensors/sensor_id/channels": CHANNELS_DATA,
+    "/v1.1/campaigns/1234/sensors/3fa85f64-5717-4562-b3fc-2c963f66afa6/channels": CHANNELS_DATA,
+    "/v1.0/campaigns/1234": CAMPAIGN_DATA_SWIM,
+    "/v1.0/campaigns/test_generic_id": CAMPAIGN_DATA_GENERIC,
+    "/v1.0/campaigns/test_swim_id": CAMPAIGN_DATA_SWIM,
+}
+
 @pytest.fixture
 def response():
     response = Mock()
@@ -38,37 +55,53 @@ def response():
         url = response._get_called_with_url
         if not url:
             return
-        elif url.endswith("Campaigns"):
-            return json.loads(json.dumps(CAMPAIGNS_DATA), **kwargs)
-        elif url.endswith("campaigns next link"):
-            return json.loads(json.dumps(CAMPAIGNS_NEXT_DATA), **kwargs)
-        # elif url.endswith("Campaigns/Type/SWIM Campaign"):
-        #     return json.loads(json.dumps(CAMPAIGNS_DATA_LIST), **kwargs)
-        # elif url.endswith("Campaigns/Type/Campaign"):
-        #     return json.loads(json.dumps(CAMPAIGNS_DATA_LIST), **kwargs)
-        elif url.lower().endswith("events"):
-            return json.loads(json.dumps(EVENTS_DATA), **kwargs)
-        elif url.lower().endswith("sensors"):
-            return json.loads(json.dumps(SENSORS_DATA), **kwargs)
-        elif url.lower().endswith("lowerstack"):
-            return json.loads(json.dumps(LOWERSTACK_DATA), **kwargs)
-        elif url.lower().endswith("/campaigns/swimops"):
-            return json.loads(json.dumps(SWIMOPS_DATA), **kwargs)
-        elif url.lower().endswith("swimops") and not url.lower().endswith("/campaigns/swimops"):
-            return json.loads(json.dumps(SWIMOPS_CAMPAIGN_DATA), **kwargs)
-        elif url.lower().endswith("campaigns/test_swim_id"):
-            return json.loads(json.dumps(CAMPAIGN_DATA_SWIM), **kwargs)
-        elif url.lower().endswith("campaigns/test_generic_id"):
-            return json.loads(json.dumps(CAMPAIGN_DATA_GENERIC), **kwargs)
-        elif url.lower().endswith("campaigns/1234"):
-            return json.loads(json.dumps(CAMPAIGN_DATA_SWIM), **kwargs)
-        elif url.lower().endswith("channels"):
-            return json.loads(json.dumps(CHANNELS_DATA), **kwargs)
-        elif url.lower().endswith("logs"):
-            return json.loads(json.dumps(LOGS_DATA), **kwargs)
+
+        return json.loads(json.dumps(DATA_MAP[url.lower()]), **kwargs)
 
     response.json.side_effect = json_side_effect
     return response
+
+
+# @pytest.fixture
+# def response():
+#     response = Mock()
+#     response._get_called_with_url = None
+
+#     def json_side_effect(*args, **kwargs):
+#         url = response._get_called_with_url
+#         if not url:
+#             return
+#         elif url.endswith("Campaigns"):
+#             return json.loads(json.dumps(CAMPAIGNS_DATA), **kwargs)
+#         elif url.endswith("campaigns next link"):
+#             return json.loads(json.dumps(CAMPAIGNS_NEXT_DATA), **kwargs)
+#         # elif url.endswith("Campaigns/Type/SWIM Campaign"):
+#         #     return json.loads(json.dumps(CAMPAIGNS_DATA_LIST), **kwargs)
+#         # elif url.endswith("Campaigns/Type/Campaign"):
+#         #     return json.loads(json.dumps(CAMPAIGNS_DATA_LIST), **kwargs)
+#         elif url.lower().endswith("events"):
+#             return json.loads(json.dumps(EVENTS_DATA), **kwargs)
+#         elif url.lower().endswith("sensors"):
+#             return json.loads(json.dumps(SENSORS_DATA), **kwargs)
+#         elif url.lower().endswith("lowerstack"):
+#             return json.loads(json.dumps(LOWERSTACK_DATA), **kwargs)
+#         elif url.lower().endswith("/campaigns/swimops"):
+#             return json.loads(json.dumps(SWIMOPS_DATA), **kwargs)
+#         elif url.lower().endswith("swimops") and not url.lower().endswith("/campaigns/swimops"):
+#             return json.loads(json.dumps(SWIMOPS_CAMPAIGN_DATA), **kwargs)
+#         elif url.lower().endswith("campaigns/test_swim_id"):
+#             return json.loads(json.dumps(CAMPAIGN_DATA_SWIM), **kwargs)
+#         elif url.lower().endswith("campaigns/test_generic_id"):
+#             return json.loads(json.dumps(CAMPAIGN_DATA_GENERIC), **kwargs)
+#         elif url.lower().endswith("campaigns/1234"):
+#             return json.loads(json.dumps(CAMPAIGN_DATA_SWIM), **kwargs)
+#         elif url.lower().endswith("channels"):
+#             return json.loads(json.dumps(CHANNELS_DATA), **kwargs)
+#         elif url.lower().endswith("logs"):
+#             return json.loads(json.dumps(LOGS_DATA), **kwargs)
+
+#     response.json.side_effect = json_side_effect
+#     return response
 
 
 @pytest.fixture
