37fb65e79b6851230e53a7be2eb6d149a5e3cef9
anna-follestad-4ss <72202484+anna-follestad-4ss@users.noreply.github.com>
2022-12-13 10:11:56 +0100

    Roll out workflow for automatic release notes and deployment. Include pull request template and labels. (#27)

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import inspect
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -25,10 +27,10 @@ _TEMPLATE_VERSION = "2.0.0"
 project = "fourinsight-campaigns"
 copyright = f"{date.today().year}, 4Subsea"
 author = "4Subsea"
+github_repo = "https://github.com/4Subsea/fourinsight-campaigns-python/"
 
 # The full version, including alpha/beta/rc tags
 version = metadata.version(project)
-# version = "0.0.1"
 release = version
 
 
@@ -42,8 +44,33 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.linkcode",
 ]
 autosummary_generate = True
+
+
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+    if not info["module"]:
+        return None
+
+    obj = sys.modules[info["module"]]
+
+    for part in info["fullname"].split("."):
+        obj = getattr(obj, part)
+    obj = inspect.unwrap(obj)
+
+    try:
+        path = os.path.relpath(inspect.getfile(obj))
+        src, lineno = inspect.getsourcelines(obj)
+
+        path = f"{github_repo}blob/main/{path}#L{lineno}-L{lineno + len(src) - 1}"
+
+    except Exception:
+        path = None
+    return path
+
 
 # Napoleon settings
 napoleon_google_docstring = False
@@ -88,7 +115,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/4Subsea/fourinsight-campaigns-python",
+            "url": github_repo,
             "icon": "fab fa-github",
         },
         {
