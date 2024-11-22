52eb7a0323d2bb5ca2457ed5301e170f8866428a
Pamela Joslyn <65171656+pamela-joslyn-4ss@users.noreply.github.com>
2024-11-22 15:32:32 +0100

    Fix style problem

--- fourinsight/campaigns/api.py
+++ fourinsight/campaigns/api.py
@@ -439,10 +439,8 @@ class CampaignsAPI:
         }
 
         response = self._get_payload(self._url(f"/{campaign_id}/Swimops"))
-
         if not response:
-            return {}
-
+            return
         response_out = _dict_rename(response[0], response_map)
         return response_out
 
